f3f5ec96befb8ccf7afd8e72a283cf8e0c7eca6e
ewhd <ewhd22@gmail.com>
2024-03-13 17:18:50 -0700

    modified early-init after testing - so far so good

--- early-init.el
+++ early-init.el
@@ -1,6 +1,5 @@
 ;; ewhd custom early-init.el -*- lexical-binding: t; -*-
-;; This file should either be placed or symlinked into ~/.emacs.d
-;; It should make emacs XDG compliant
+
 
 ;; Prerequisite for using elpaca
 ;; From https://github.com/progfolio/elpaca/blob/master/doc/early-init.el
@@ -10,12 +9,10 @@
 (defvar user-home-directory (concat (getenv "HOME") "/"))
 
 
-
 ;; Set XDG Base Directory Specification variables
 (setq user-emacs-directory (expand-file-name (concat (getenv "XDG_CONFIG_HOME") "/emacs/") user-emacs-directory))
-(setq user-emacs-cache-directory (expand-file-name (concat (getenv "XDG_CACHE_HOME") "/emacs/") user-emacs-cache-directory))
-(setq user-emacs-data-directory (expand-file-name (concat (getenv "XDG_DATA_HOME") "/emacs/") user-emacs-data-directory))
-
+(defvar user-emacs-cache-directory (expand-file-name (concat (getenv "XDG_CACHE_HOME") "/emacs/")))
+(defvar user-emacs-data-directory (expand-file-name (concat (getenv "XDG_DATA_HOME") "/emacs/")))
 
 ;; Ensure the directories exist
 (make-directory user-emacs-directory t)
@@ -23,13 +20,11 @@
 (make-directory user-emacs-data-directory t)
 
 
-;; Alternatively, if running without XDG base directories, comment the above sections and uncomment the following line:
-;; (setq user-emacs-directory (concat user-home-directory ".emacs.d/"))
-
-
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; no-native-compile: t
 ;; no-update-autoloads: t
-
 ;; End:
+
+
+;;EOF
