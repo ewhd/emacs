50db99a8b76fa0db095e13e669fc15b6fdbf465b
ewhd <ewhd22@gmail.com>
2024-05-13 00:40:11 -0700

    updates to multi-cursor behavior

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,14 +3,22 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+	beginning-of-visual-line
+	comment-line
+	end-of-visual-line
+	indent-for-tab-command
 	org-beginning-of-line
 	org-ctrl-c-ctrl-c
 	org-delete-char
 	org-end-of-line
 	org-metaright
 	org-self-insert-command
+	text-scale-increase
+	zap-up-to-char
 	))
 
 (setq mc/cmds-to-run-once
       '(
+	end-of-buffer
+	text-scale-decrease
 	))
