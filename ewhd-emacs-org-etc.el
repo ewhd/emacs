226e04e97a9de26d992354f6b43e4a73f98dad80
ewhd <ewhd22@gmail.com>
2024-09-05 14:16:54 -0700

    moves org-refile-targets after org-agenda-files is set

--- ewhd-emacs-org-etc.el
+++ ewhd-emacs-org-etc.el
@@ -81,11 +81,6 @@
    org-image-actual-width '(300)
    )
 
-;; Set list of legitimate refile targets
-(setq org-refile-targets
-      (mapcar (lambda (file) (cons file '(:maxlevel . 2)))
-              (append org-agenda-files
-                      '("~/Documents/notes/20240504T125856--backburner.org"))))
 
 ;; Allow refiling to the top (file) level, rather than just to headings (which is the default)
 (setq org-refile-use-outline-path 'file)
@@ -148,6 +143,11 @@
        'file-exists-p
        (directory-files-recursively "~/Documents" "^[^\.#].*_gtd.*\\.org$")))
 
+;; Set list of legitimate refile targets
+(setq org-refile-targets
+      (mapcar (lambda (file) (cons file '(:maxlevel . 2)))
+              (append org-agenda-files
+                      '("~/Documents/notes/20240504T125856--backburner.org"))))
 
 
 
