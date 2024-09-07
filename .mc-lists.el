2f4803d162a00cf0b4f76c4478634aecc00e34a2
ewhd <ewhd22@gmail.com>
2024-09-06 22:56:20 -0700

    updates .mc-list

--- .mc-lists.el
+++ .mc-lists.el
@@ -8,18 +8,22 @@
 	end-of-visual-line
 	eval-last-sexp
 	indent-for-tab-command
+	kill-region
 	multiple-cursors-mode
 	org-beginning-of-line
 	org-ctrl-c-ctrl-c
 	org-cycle
 	org-delete-char
 	org-end-of-line
+	org-kill-line
 	org-metaleft
 	org-metaright
 	org-open-line
 	org-self-insert-command
 	org-shiftleft
 	org-shiftright
+	org-yank
+	query-replace
 	tab-next
 	text-scale-increase
 	zap-up-to-char
