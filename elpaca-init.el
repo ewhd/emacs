b312ce135a14a2880612073354d8e923215518a2
ewhd <ewhd22@gmail.com>
2025-11-03 19:46:33 -0800

    update elpaca version (needed when loading config on new omarchy laptop)

--- elpaca-init.el
+++ elpaca-init.el
@@ -2,7 +2,7 @@
 ;; From https://github.com/progfolio/elpaca/blob/master/doc/init.el
 
 
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
