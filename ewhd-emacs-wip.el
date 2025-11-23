be69d22bb6adaeffff8f829724801eeba9c99b90
ewhd <ewhd22@gmail.com>
2025-11-22 16:09:48 -0800

    add func to swap vert/horizontal window split (wip)

--- ewhd-emacs-wip.el
+++ ewhd-emacs-wip.el
@@ -40,3 +40,19 @@
   ;; (keymap-set Man-mode-map "K" #'Man-kill)
   ;; (keymap-set Man-mode-map "o" #'casual-man-occur-options)
   )
+
+
+
+(defun ewhd-window-split-toggle ()
+  "Toggle between horizontal and vertical split with two windows."
+  (interactive)
+  (if (> (length (window-list)) 2)
+      (error "Can't toggle with more than 2 windows!")
+    (let ((func (if (window-full-height-p)
+                    #'split-window-vertically
+                  #'split-window-horizontally)))
+      (delete-other-windows)
+      (funcall func)
+      (save-selected-window
+        (other-window 1)
+        (switch-to-buffer (other-buffer))))))
